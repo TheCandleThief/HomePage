212576751446ce150db16208a2c9b0d96938ba31
Cobb <627573954@qq.com>
2018-11-02 17:17:28 +0800

    Revert: 修改程序运行入口模块

--- run.py
+++ run.py
@@ -6,16 +6,3 @@
 """
 
 __author__ = 'Cobb'
-
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
