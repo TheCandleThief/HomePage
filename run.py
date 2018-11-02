f442cb0ab945ed65992352c5d5af098df2d8e8b1
Cobb <627573954@qq.com>
2018-11-02 15:32:52 +0800

    修改程序运行入口模块

--- run.py
+++ run.py
@@ -6,3 +6,16 @@
 """
 
 __author__ = 'Cobb'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
